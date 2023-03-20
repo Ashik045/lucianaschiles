894c3957ef2a515f8b19b0c49017f4d587cb79c6
Ashik045 <ashikurislam045@gmail.com>
2023-03-20 21:07:27 +0600

    navbar & header responsive done

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -5,6 +5,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 // import required modules
 import Image from 'next/image';
+import { FaGift, FaHeadset, FaMoneyCheck, FaShippingFast } from 'react-icons/fa';
 import { Autoplay, EffectFade, Pagination } from 'swiper';
 import header1 from '../../images/header1.jpg';
 import header2 from '../../images/header2.jpg';
@@ -12,26 +13,52 @@ import header3 from '../../images/header3.jpg';
 import header4 from '../../images/header4.jpg';
 import styles from './header.module.scss';
 
+const headerBottomDetails = [
+    {
+        id: 1,
+        title: 'Fast & Free Shipping Worldwide',
+        subTitle: 'Your Order Over $50',
+        icon: <FaShippingFast />,
+    },
+    {
+        id: 2,
+        title: 'Order Return Within 7 Days',
+        subTitle: 'Money Back Guarantee!',
+        icon: <FaMoneyCheck />,
+    },
+    {
+        id: 3,
+        title: '24/7 Online Customer Support',
+        subTitle: 'Anytime Need Help?',
+        icon: <FaHeadset />,
+    },
+    {
+        id: 4,
+        title: 'Gift Voucher On Your First Order',
+        subTitle: 'Collect Free Gift Voucher',
+        icon: <FaGift />,
+    },
+];
 
-const Header = () => {
-  return (
-    <div className={styles.header}>
+function Header() {
+    return (
+        <div className={styles.header}>
             <div className={styles.header_main}>
                 <Swiper
                     spaceBetween={30}
                     pagination={{
-                      clickable: true,
+                        clickable: true,
                     }}
-                    effect={"fade"}
+                    effect="fade"
                     modules={[EffectFade, Pagination, Autoplay]}
                     loop
                     autoplay={{
-                        delay: 2500,
+                        delay: 2000,
                         disableOnInteraction: false,
                     }}
                     className="mySwiper"
                 >
-                    <SwiperSlide  className={styles.slidee}>
+                    <SwiperSlide className={styles.slidee}>
                         <Image
                             src={header1}
                             alt="jewelry img"
@@ -41,7 +68,7 @@ const Header = () => {
                             width={1250}
                         />
                     </SwiperSlide>
-                    <SwiperSlide  className={styles.slidee}>
+                    <SwiperSlide className={styles.slidee}>
                         <Image
                             src={header2}
                             alt="jewelry img"
@@ -51,7 +78,7 @@ const Header = () => {
                             layout="responsive"
                         />
                     </SwiperSlide>
-                    <SwiperSlide  className={styles.slidee}>
+                    <SwiperSlide className={styles.slidee}>
                         <Image
                             src={header3}
                             alt="jewelry img"
@@ -61,7 +88,7 @@ const Header = () => {
                             layout="responsive"
                         />
                     </SwiperSlide>
-                    <SwiperSlide  className={styles.slidee}>
+                    <SwiperSlide className={styles.slidee}>
                         <Image
                             src={header4}
                             alt="jewelry img"
@@ -73,8 +100,23 @@ const Header = () => {
                     </SwiperSlide>
                 </Swiper>
             </div>
+
+            <div className={styles.header_bottom}>
+                {headerBottomDetails.map((details) => (
+                    <div
+                        key={details.id}
+                        className={
+                            details.id === 4 ? styles.header_btm_itemm : styles.header_btm_item
+                        }
+                    >
+                        <span>{details.icon}</span>
+                        <h4>{details.title}</h4>
+                        <p>{details.subTitle}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-  )
+    );
 }
 
-export default Header
+export default Header;
--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,42 +1,42 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { FaShoppingCart, FaStream, FaTimes } from 'react-icons/fa';
 import styles from './navbar.module.scss';
 
-const Navbar = () => {
-
-  const router = useRouter();
+function Navbar() {
+    const router = useRouter();
     const [toggler, setToggler] = useState(false);
     const [cartItem, setCartItem] = useState(0);
 
     useEffect(() => {
-        const items = typeof window !== 'undefined' && JSON.parse(localStorage.getItem('productlist'))
-        setCartItem(items?.length)
+        const items =
+            typeof window !== 'undefined' && JSON.parse(localStorage.getItem('productlist'));
+        setCartItem(items?.length);
         // console.log(cartItem);
-    }, [cartItem])
+    }, [cartItem]);
 
     const handleToggle = () => {
         setToggler(!toggler);
     };
 
-  return (
-    <div className={styles.navbar}>
-      <div className={styles.navbar_main}>
-            <Link href="/" style={{textDecoration: 'none'}}>
-                <div className={styles.brand}>
-                    <h3>Lucianaschiles</h3>
-                </div>
-            </Link>
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.navbar_main}>
+                <Link href="/" style={{ textDecoration: 'none' }}>
+                    <div className={styles.brand}>
+                        <h3>Lucianaschiles</h3>
+                    </div>
+                </Link>
 
-            <div className={styles.menus}>
+                <div className={styles.menus}>
                     <div className={styles.menu_item}>
-                        <Link href="/" style={{textDecoration: 'none'}}>
+                        <Link href="/" style={{ textDecoration: 'none' }}>
                             <p className={router.pathname === '/' ? `${styles.active}` : ''}>
                                 Home
                             </p>
                         </Link>
-                        <Link href="/products" style={{textDecoration: 'none'}}>
+                        <Link href="/products" style={{ textDecoration: 'none' }}>
                             <p
                                 className={
                                     router.pathname === '/products' ? `${styles.active}` : ''
@@ -45,55 +45,51 @@ const Navbar = () => {
                                 Shop
                             </p>
                         </Link>
-                        <Link href="/media" style={{textDecoration: 'none'}}>
-                            <p
-                                className={
-                                    router.pathname === '/media' ? `${styles.active}` : ''
-                                }
-                            >
+                        <Link href="/media" style={{ textDecoration: 'none' }}>
+                            <p className={router.pathname === '/media' ? `${styles.active}` : ''}>
                                 Media
                             </p>
                         </Link>
-                        <Link href="/blogs" style={{textDecoration: 'none'}}>
+                        <Link href="/blogs" style={{ textDecoration: 'none' }}>
                             <p className={router.pathname === '/blogs' ? `${styles.active}` : ''}>
                                 Blogs
                             </p>
                         </Link>
-                        <Link href="/about" style={{textDecoration: 'none'}}>
+                        <Link href="/about" style={{ textDecoration: 'none' }}>
                             <p className={router.pathname === '/about' ? `${styles.active}` : ''}>
                                 About Us
                             </p>
                         </Link>
 
-                        <Link href="/contact" style={{textDecoration: 'none'}}>
+                        <Link href="/contact" style={{ textDecoration: 'none' }}>
                             <p className={router.pathname === '/contact' ? `${styles.active}` : ''}>
                                 Contact
                             </p>
                         </Link>
                     </div>
-            </div>
-                
+                </div>
+
                 <div className={styles.cart}>
-                        <Link href="/cart" style={{textDecoration: 'none'}}>
-                                <FaShoppingCart className={styles.cart_icon} />
-                                <span>{cartItem || 0}</span>
-                        </Link>
-                    </div>
+                    <Link href="/cart" style={{ textDecoration: 'none' }}>
+                        <FaShoppingCart className={styles.cart_icon} />
+                        <span>{cartItem || 0}</span>
+                    </Link>
+                </div>
 
                 {toggler ? (
                     <FaTimes onClick={handleToggle} className={styles.toggle_icon} size={22} />
                 ) : (
-                    <FaStream onClick={handleToggle} className={styles.toggle_icon}  />
+                    <FaStream onClick={handleToggle} className={styles.toggle_icon} />
                 )}
                 {toggler && (
                     <div className={styles.res_menu}>
                         <div className={styles.res_menu_item}>
-                            <Link href="/" style={{textDecoration: 'none'}}>
+                            <Link href="/" style={{ textDecoration: 'none' }}>
                                 <p className={router.pathname === '/' ? `${styles.active}` : ''}>
                                     Home
                                 </p>
                             </Link>
-                            <Link href="/products" style={{textDecoration: 'none'}}>
+                            <Link href="/products" style={{ textDecoration: 'none' }}>
                                 <p
                                     className={
                                         router.pathname === '/products' ? `${styles.active}` : ''
@@ -102,7 +98,7 @@ const Navbar = () => {
                                     Products
                                 </p>
                             </Link>
-                            <Link href="/about" style={{textDecoration: 'none'}}>
+                            <Link href="/about" style={{ textDecoration: 'none' }}>
                                 <p
                                     className={
                                         router.pathname === '/about' ? `${styles.active}` : ''
@@ -111,7 +107,7 @@ const Navbar = () => {
                                     About
                                 </p>
                             </Link>
-                            <Link href="/blogs" style={{textDecoration: 'none'}}>
+                            <Link href="/blogs" style={{ textDecoration: 'none' }}>
                                 <p
                                     className={
                                         router.pathname === '/blogs' ? `${styles.active}` : ''
@@ -123,9 +119,9 @@ const Navbar = () => {
                         </div>
                     </div>
                 )}
-      </div>
-    </div>
-  )
+            </div>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
